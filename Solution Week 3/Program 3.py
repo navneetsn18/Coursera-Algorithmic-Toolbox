181e6ae3e9a9a1076af6c2b8363217efef02b727
navneetsn18 <navneetsn18@gmail.com>
2020-06-02 18:57:37 +0530

    Adding solution for partial equal subset sum from leetcode

--- Solution Week 3/Program 3.py
+++ Solution Week 3/Program 3.py
@@ -1,10 +1,21 @@
-# python3
 import sys
-
-
 def compute_min_refills(distance, tank, stops):
-    # write your code here
-    return -1
+    stops=[0]+stops
+    stops.append(distance)
+    fuel=tank
+    steps=0
+    dist_stop=[]
+    for i in range(len(stops)-1):
+        dist_stop.append(stops[i+1]-stops[i])
+    for i in range(len(dist_stop)):
+        if(dist_stop[i]>tank):
+            return -1
+        fuel-=dist_stop[i]
+        if(fuel<=0):
+            steps+=1
+            i-=1 
+            fuel=tank
+    return steps
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())
